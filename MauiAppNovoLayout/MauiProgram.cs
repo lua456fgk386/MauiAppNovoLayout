7a14fd14516322baae5b802a5dd76f8771b9f95a
Douglas <douglas_nunes16@hotmail.com>
2026-05-25 01:27:09 -0300

    add recusos ao app

--- MauiAppNovoLayout/MauiProgram.cs
+++ MauiAppNovoLayout/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace MauiAppNovoLayout
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Caveat-Bold.ttf", "CaveatBold");
+                    fonts.AddFont("Caveat-Medium.ttf", "CaveatMedium");
+                    fonts.AddFont("Caveat-Regular.ttf", "CaveatRegular");
+                    fonts.AddFont("Caveat-SemiBold.ttf", "CaveatSemiBold");
                 });
 
 #if DEBUG
